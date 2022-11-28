bf6c3ec4f15e1a8c002d04d88136ba58f0812031
tnty <tnty.working@gmail.com>
2022-11-28 19:51:05 +0300

    feat: how-it-work section

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,13 +2,16 @@ import React, { FC } from "react";
 
 import Section from "@/components/ui/Section/Section";
 
+import HowItWorkContainer from "@/containers/HowItWorkContainer/HowItWorkContainer";
 import WelcomeContainer from "@/containers/WelcomeContainer/WelcomeContainer";
 
 const MainPage: FC = () => {
     return (
         <>
             <WelcomeContainer />
-            <Section title={"Как это работает?"}></Section>
+            <Section title={"Как это работает?"}>
+                <HowItWorkContainer />
+            </Section>
         </>
     );
 };
