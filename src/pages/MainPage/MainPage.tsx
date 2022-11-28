fd783518b4e40274f285a9781a3a3b003aa348c6
tnty <tnty.working@gmail.com>
2022-11-28 19:20:23 +0300

    feat: reused component section

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from "react";
 
+import Section from "@/components/ui/Section/Section";
+
 import WelcomeContainer from "@/containers/WelcomeContainer/WelcomeContainer";
 
 const MainPage: FC = () => {
     return (
         <>
             <WelcomeContainer />
+            <Section title={"Как это работает?"}></Section>
         </>
     );
 };
