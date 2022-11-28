8c3ebe85f5bc2e96035c1776a558747b9822e20a
tnty <tnty.working@gmail.com>
2022-11-28 15:21:25 +0300

    feat: section title component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,12 @@ import React, { FC } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import Button from "@/components/ui/Button/Button";
+import SectionTitle from "@/components/ui/SectionTitle/SectionTitle";
 
 const App: FC = () => {
     return (
         <BrowserRouter>
+            <SectionTitle>Работа</SectionTitle>
             <Button path={"/"} type={"primary"}>
                 Посмотреть все
             </Button>
