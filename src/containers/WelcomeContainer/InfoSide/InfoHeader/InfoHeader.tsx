9d2e75c3eca38cd4fc8dc2c031975b97892f466c
tnty <tnty.working@gmail.com>
2022-11-29 20:01:08 +0300

    feat: burger menu

--- src/containers/WelcomeContainer/InfoSide/InfoHeader/InfoHeader.tsx
+++ src/containers/WelcomeContainer/InfoSide/InfoHeader/InfoHeader.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from "react";
+import cn from "classnames";
+import React, { FC, useState } from "react";
 import { Link } from "react-router-dom";
 
 import ChangeLanguage from "@/components/ChangeLanguage/ChangeLanguage";
@@ -8,25 +9,57 @@ import Button from "@/components/ui/Button/Button";
 import styles from "./InfoHeader.module.scss";
 
 const InfoHeader: FC = () => {
+    const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
     return (
-        <header className={styles.header}>
-            <ChangeLanguage />
-            <div className={styles.link}>
-                <Link to={"/"}>Как это работает?</Link>
+        <>
+            <div
+                className={cn(styles.menu, {
+                    [styles.active]: menuIsOpen,
+                })}
+            >
+                <div className={styles.link}>
+                    <Link to={"/"}>Как это работает?</Link>
+                </div>
+                <div className={styles.link}>
+                    <Link to={"/"}>Cообщество</Link>
+                    <Arrow isOpen={false} />
+                </div>
+                <div className={styles.link}>
+                    <Link to={"/"}>Вход</Link>
+                </div>
+                <div className={styles.link}>
+                    <Link to={"/"}>Регистрация</Link>
+                </div>
             </div>
-            <div className={styles.link}>
-                <Link to={"/"}>Cообщество</Link>
-                <Arrow isOpen={false} />
-            </div>
-            <div className={styles.link}>
-                <Link to={"/"}>Вход</Link>
-            </div>
-            <div className={styles.link}>
-                <Button className={styles.btn} type={"outlined"} path={"/"}>
-                    Регистрация
-                </Button>
-            </div>
-        </header>
+            <header className={styles.header}>
+                <ChangeLanguage />
+                <div className={styles.link}>
+                    <Link to={"/"}>Как это работает?</Link>
+                </div>
+                <div className={styles.link}>
+                    <Link to={"/"}>Cообщество</Link>
+                    <Arrow isOpen={false} />
+                </div>
+                <div className={styles.link}>
+                    <Link to={"/"}>Вход</Link>
+                </div>
+                <div className={styles.link}>
+                    <Button className={styles.btn} type={"outlined"} path={"/"}>
+                        Регистрация
+                    </Button>
+                </div>
+                <div
+                    className={cn(styles.burger, {
+                        [styles.open]: menuIsOpen,
+                    })}
+                    onClick={() => setMenuIsOpen(!menuIsOpen)}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </header>
+        </>
     );
 };
 
