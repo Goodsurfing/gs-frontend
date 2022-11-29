6788da6b7e1b701ba33d02c14881e983b3643f4b
tnty <tnty.working@gmail.com>
2022-11-29 10:14:30 +0300

    feat: benefits section

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 
 import Section from "@/components/ui/Section/Section";
 
+import BenefitsContainer from "@/containers/BenefitsContainer/BenefitsContainer";
 import HowItWorkContainer from "@/containers/HowItWorkContainer/HowItWorkContainer";
 import OffersContainer from "@/containers/OffersContainer/OffersContainer";
 import WelcomeContainer from "@/containers/WelcomeContainer/WelcomeContainer";
@@ -15,6 +16,9 @@ const MainPage: FC = () => {
             </Section>
             <Section title={"Интересные предложения"}>
                 <OffersContainer />
+            </Section>
+            <Section title={"Наши преимущества"}>
+                <BenefitsContainer />
             </Section>
         </>
     );
