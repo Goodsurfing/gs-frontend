05e7950831c4bad13e9623315cc96c92fec0ba90
tnty <tnty.working@gmail.com>
2022-11-30 16:07:41 +0300

    feat: empty header component

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,11 +1,15 @@
-import React, { FC } from 'react';
-import styles from './SignUpPage.module.scss';
+import React, { FC } from "react";
+
+import EmptyHeader from "@/components/ui/EmptyHeader/EmptyHeader";
+
+import styles from "./SignUpPage.module.scss";
 
 const SignUpPage: FC = () => {
-
-  return (
-      <main className={styles.main}></main>
-  );
+    return (
+        <main className={styles.main}>
+            <EmptyHeader />
+        </main>
+    );
 };
 
 export default SignUpPage;
--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,4 +1,4 @@
 export enum AppRoutesEnum {
     HOME = "/",
-    SIGNUP = "/signup"
+    SIGNUP = "signup",
 }
