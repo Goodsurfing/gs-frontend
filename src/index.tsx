efec2af8664c39ce6c88dd9205be87ef90c8e164
tnty <tnty.working@gmail.com>
2022-12-01 17:38:39 +0300

    feat: base redux config

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import App from "@/app/App";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+
+import { setupStore } from "@/store/store";
 
 import "./styles/index.scss";
 
 const root = createRoot(document.getElementById("root")!);
 
+const store = setupStore();
+
 const Root = (
-    <>
+    <Provider store={store}>
         <App />
-    </>
+    </Provider>
 );
 
 root.render(Root);
