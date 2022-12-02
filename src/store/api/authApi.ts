c6592a9ae718fb0b54b748ae45c9dcb1c70d1c6d
tnty <tnty.working@gmail.com>
2022-12-02 16:32:11 +0300

    refactor: back url

--- src/store/api/authApi.ts
+++ src/store/api/authApi.ts
@@ -2,10 +2,11 @@ import { AuthApiEndpoints, IAuthFormData } from "@/type/auth/auth.interface";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 import { IGenericResponse } from "@/store/api/api.types";
+import {API_BASE_URL} from "@/constants/api";
 
 export const authApi = createApi({
     reducerPath: "authApi",
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.BACKEND_BASE_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
     endpoints: (build) => ({
         registerUser: build.mutation<IGenericResponse, IAuthFormData>({
             query: (data) => ({
