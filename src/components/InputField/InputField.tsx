872564e7faab57d6fb6e8102b1d10083fe23826e
tnty <tnty.working@gmail.com>
2022-12-02 14:42:43 +0300

    feat: get data from form

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,5 +1,5 @@
 import cn from "classnames";
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 
 import styles from "./InputField.module.scss";
 
@@ -8,19 +8,19 @@ interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
     text: string;
 }
 
-const InputField: FC<InputFieldProps> = ({ type, text, ...rest }) => {
-    const [value, setValue] = useState<string>("");
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value);
-    };
-
+const InputField: FC<InputFieldProps> = ({
+    type,
+    text,
+    value,
+    onChange,
+    ...rest
+}) => {
     return (
         <div className={styles.box}>
             <input
                 type={type}
                 required={true}
-                onChange={handleChange}
+                onChange={onChange}
                 value={value}
                 {...rest}
             />
--- src/containers/SignUpContainer/SignUpForm/SignUpForm.tsx
+++ src/containers/SignUpContainer/SignUpForm/SignUpForm.tsx
@@ -1,6 +1,7 @@
 import { IAuthFormData } from "@/type/auth/auth.interface";
 import React, { FC } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { Controller } from "react-hook-form";
 
 import InputField from "@/components/InputField/InputField";
 import Button from "@/components/ui/Button/Button";
@@ -8,7 +9,7 @@ import Button from "@/components/ui/Button/Button";
 import styles from "./SignUpForm.module.scss";
 
 const SignUpForm: FC = () => {
-    const { register, reset, handleSubmit } = useForm<IAuthFormData>({
+    const { control, reset, handleSubmit } = useForm<IAuthFormData>({
         mode: "onChange",
     });
 
@@ -19,8 +20,32 @@ const SignUpForm: FC = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-            <InputField type={"email"} text={"E-mail"} />
-            <InputField type={"password"} text={"Пароль"} />
+            <Controller
+                control={control}
+                name={"email"}
+                defaultValue={""}
+                render={({ field }) => (
+                    <InputField
+                        onChange={(e) => field.onChange(e)}
+                        value={field.value}
+                        type={"email"}
+                        text={"E-mail"}
+                    />
+                )}
+            />
+            <Controller
+                control={control}
+                name={"password"}
+                defaultValue={""}
+                render={({ field }) => (
+                    <InputField
+                        onChange={(e) => field.onChange(e)}
+                        value={field.value}
+                        type={"password"}
+                        text={"Пароль"}
+                    />
+                )}
+            />
             <Button type={"submit"} variant={"primary"} className={styles.btn}>
                 Зарегистрироваться
             </Button>
