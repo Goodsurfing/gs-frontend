0db744b9d4712198e0fb00221fe8d8a23a5f1a72
Ravil Sharafutdinov <tnty.working@gmail.com>
2022-12-05 14:40:05 +0300

    feat: get data from reset password request form

--- src/containers/ResetPasswordContainer/ResetPasswordFirstStep/ResetPasswordFirstStep.tsx
+++ src/containers/ResetPasswordContainer/ResetPasswordFirstStep/ResetPasswordFirstStep.tsx
@@ -1,4 +1,6 @@
+import { IResetPasswordRequestFormData } from "@/type/auth/auth.interface";
 import React, { FC } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 
 import InputField from "@/components/InputField/InputField";
 import Button from "@/components/ui/Button/Button";
@@ -6,11 +8,36 @@ import Button from "@/components/ui/Button/Button";
 import styles from "./ResetPasswordFirstStep.module.scss";
 
 const ResetPasswordFirstStep: FC = () => {
-    return (
-        <form className={styles.form}>
-            <InputField type={"email"} text={"E-mail"} />
+    const { control, reset, handleSubmit } =
+        useForm<IResetPasswordRequestFormData>({
+            mode: "onChange",
+        });
 
-            <Button variant={"primary"}>Отправить</Button>
+    const onSubmit: SubmitHandler<IResetPasswordRequestFormData> = async (
+        data
+    ) => {
+        console.log(data);
+        reset();
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+            <Controller
+                control={control}
+                name={"email"}
+                defaultValue={""}
+                render={({ field }) => (
+                    <InputField
+                        onChange={(e) => field.onChange(e)}
+                        value={field.value}
+                        type={"email"}
+                        text={"E-mail"}
+                    />
+                )}
+            />
+            <Button type={"submit"} variant={"primary"}>
+                Отправить
+            </Button>
         </form>
     );
 };
