e4334108f4320ec187e0417c455d046205579ce4
Ravil Sharafutdinov <tnty.working@gmail.com>
2022-12-06 10:29:15 +0300

    feat: i18n init

---
+++ src/i18n.ts
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n.use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: "ru",
+        detection: {
+            order: ["querystring", "cookie", "path", "navigator"],
+            caches: ["cookie"],
+        },
+        interpolation: {
+            escapeValue: false,
+        },
+    });
+
+export default i18n;
