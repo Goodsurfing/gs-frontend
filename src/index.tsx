ae76637985ceb42523da68708fe9b3f073d515ff
Ravil Sharafutdinov <tnty.working@gmail.com>
2022-12-06 10:50:47 +0300

    feat: mock locales

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,23 @@
 import App from "@/app/App";
-import React from "react";
+import React, {Suspense} from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import { setupStore } from "@/store/store";
 
 import "./styles/index.scss";
+import "./i18n";
 
 const root = createRoot(document.getElementById("root")!);
 
 const store = setupStore();
 
 const Root = (
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <Suspense fallback={<div>Loading...</div>}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Suspense>
 );
 
 root.render(Root);
