87e38643ae244481f7363b07c1ca805394fc4097
Ravil Sharafutdinov <tnty.working@gmail.com>
2022-12-15 10:32:46 +0300

    feat: private routes to profile pages

---
+++ src/pages/ProfilePages/ProfileInfoPage/ProfileInfoPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface ProfileInfoPageProps {}
+
+const ProfileInfoPage: FC<ProfileInfoPageProps> = () => {
+    return <div>Profile page</div>;
+};
+
+export default ProfileInfoPage;
