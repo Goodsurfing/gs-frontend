5e66ee926dd7c5c977ef2cbd88cdb0afb25c8599
Ravil Sharafutdinov <tnty.working@gmail.com>
2022-12-16 11:20:55 +0300

    feat: decompose support widget

---
+++ src/components/SupportWidget/SupportWidget.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+
+import Button from "@/components/ui/Button/Button";
+
+import supportImage from "@/assets/images/supportImage.jpg";
+
+import styles from "./SupportWidget.module.scss";
+
+const SupportWidget: FC = () => {
+    return (
+        <aside className={styles.support}>
+            <img src={supportImage} alt="Кристина" />
+            <h4 className={styles.name}>Кристина</h4>
+            <p className={styles.description}>
+                Ваш персональный помощник по работе с системой
+            </p>
+            <Button variant={"outlined"}>Написать</Button>
+            <div className={styles.email}>
+                <p>E-mail</p>
+                <a href="#" className={styles.address}>
+                    support@goodsurfing.org
+                </a>
+            </div>
+        </aside>
+    );
+};
+
+export default SupportWidget;
