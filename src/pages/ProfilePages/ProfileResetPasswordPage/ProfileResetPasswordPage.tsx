27568dff757bc389767695378314173a55090160
Ravil Sharafutdinov <tnty.working@gmail.com>
2022-12-20 13:26:57 +0300

    feat: profile reset password page layour

--- src/pages/ProfilePages/ProfileResetPasswordPage/ProfileResetPasswordPage.tsx
+++ src/pages/ProfilePages/ProfileResetPasswordPage/ProfileResetPasswordPage.tsx
@@ -1,7 +1,20 @@
 import React, { FC } from "react";
 
+import ProfileResetPasswordForm from "@/pages/ProfilePages/ProfileResetPasswordPage/ProfileResetPasswordForm/ProfileResetPasswordForm";
+
+import styles from "./ProfileResetPasswordPage.module.scss";
+
 const ProfileResetPasswordPage: FC = () => {
-    return <div>reset password</div>;
+    return (
+        <main className={styles.main}>
+            <div className={styles.title}>
+                <h2>Изменение пароля</h2>
+            </div>
+            <div className={styles.form}>
+                <ProfileResetPasswordForm />
+            </div>
+        </main>
+    );
 };
 
 export default ProfileResetPasswordPage;
