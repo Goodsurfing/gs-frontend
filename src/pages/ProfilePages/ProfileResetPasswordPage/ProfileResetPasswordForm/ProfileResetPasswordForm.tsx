7c98f53046ea39e503b18c2c964ddf606cee3bbe
Ravil Sharafutdinov <tnty.working@gmail.com>
2022-12-23 13:12:45 +0300

    feat: get data from form

--- src/pages/ProfilePages/ProfileResetPasswordPage/ProfileResetPasswordForm/ProfileResetPasswordForm.tsx
+++ src/pages/ProfilePages/ProfileResetPasswordPage/ProfileResetPasswordForm/ProfileResetPasswordForm.tsx
@@ -4,32 +4,37 @@ import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import InputField from "@/components/InputField/InputField";
 import Button from "@/components/ui/Button/Button";
 
+import { useAppSelector } from "@/hooks/redux";
+
+import { authApi } from "@/store/api/authApi";
+
 import styles from "./ProfileResetPasswordForm.module.scss";
 
+interface IResetPasswordFormFields {
+    newPassword: string;
+    confirmNewPassword: string;
+}
+
 const ProfileResetPasswordForm: FC = () => {
-    const { control, handleSubmit } = useForm({
+    const { token } = useAppSelector((state) => state.login);
+
+    const { control, handleSubmit } = useForm<IResetPasswordFormFields>({
         mode: "onChange",
     });
 
-    const onSubmit: SubmitHandler<any> = (data) => {
-        console.log(data);
+    const onSubmit: SubmitHandler<IResetPasswordFormFields> = ({
+        newPassword,
+        confirmNewPassword,
+    }) => {
+        if (newPassword && confirmNewPassword && token) {
+            if (newPassword === confirmNewPassword) {
+                console.log("ok");
+            }
+        }
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-            <Controller
-                control={control}
-                name={"oldPassword"}
-                defaultValue={""}
-                render={({ field }) => (
-                    <InputField
-                        onChange={(e) => field.onChange(e)}
-                        value={field.value}
-                        text={"Старый пароль"}
-                        type={"text"}
-                    />
-                )}
-            />
             <Controller
                 control={control}
                 name={"newPassword"}
@@ -39,6 +44,19 @@ const ProfileResetPasswordForm: FC = () => {
                         onChange={(e) => field.onChange(e)}
                         value={field.value}
                         text={"Новый пароль"}
+                        type={"text"}
+                    />
+                )}
+            />
+            <Controller
+                control={control}
+                name={"confirmNewPassword"}
+                defaultValue={""}
+                render={({ field }) => (
+                    <InputField
+                        onChange={(e) => field.onChange(e)}
+                        value={field.value}
+                        text={"Подтвердите пароль"}
                         type={"text"}
                     />
                 )}
