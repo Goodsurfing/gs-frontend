7f7cda9491f56f5e1681866467fdcf7a65ba2c9d
kuromukade <kuromukade@mail.ru>
2023-02-16 10:24:15 +0300

    cn fix

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -7,17 +7,19 @@ import { ISideMenu } from "./types/SideMenu.interface";
 
 const SideMenu: FC<ISideMenu> = ({ theme, children }) => {
     const [isOpen, setOpen] = useState<boolean>(false);
-    const classname = cn(styles.sideMenu, {
-        [styles.dark]: theme === "DARK",
-        [styles.white]: theme === "LIGHT",
-    });
 
     return (
         <aside
-            style={{ backgroundColor: theme }}
-            className={cn(classname, {
-                [styles.open]: isOpen,
-            })}
+            className={cn(
+                styles.sideMenu,
+                {
+                    [styles.dark]: theme === "DARK",
+                    [styles.white]: theme === "LIGHT",
+                },
+                {
+                    [styles.open]: isOpen,
+                }
+            )}
         >
             {children}
             <SideMenuArrow theme={theme} opened={isOpen} setOpened={setOpen} />
--- src/components/SideMenu/SideMenuArrow/SideMenuArrow.tsx
+++ src/components/SideMenu/SideMenuArrow/SideMenuArrow.tsx
@@ -7,25 +7,25 @@ import { ISideMenuArrow } from "../types/SideMenuArrow.interface";
 import styles from "./SideMenuArrow.module.scss";
 
 const SideMenuArrow: FC<ISideMenuArrow> = ({ setOpened, opened, theme }) => {
-    const boxClassname = cn(styles.box, {
-        [styles.boxOpened]: opened,
-    });
-
-    const arrowClassname = cn({
-        [styles.opened]: opened === true,
-        [styles.closed]: opened === false,
-    });
-
     return (
         <div
             onClick={() => setOpened(!opened)}
-            className={cn(boxClassname, {
-                [styles.dark]: theme === "DARK",
-                [styles.light]: theme === "LIGHT",
-            })}
+            className={cn(
+                styles.box,
+                {
+                    [styles.boxOpened]: opened,
+                },
+                {
+                    [styles.dark]: theme === "DARK",
+                    [styles.light]: theme === "LIGHT",
+                }
+            )}
         >
             <img
-                className={arrowClassname}
+                className={cn({
+                    [styles.opened]: opened === true,
+                    [styles.closed]: opened === false,
+                })}
                 src={arrow}
                 alt="arrow"
                 loading="lazy"
