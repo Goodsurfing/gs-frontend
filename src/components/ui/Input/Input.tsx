3abf5e8edd485c0fe606ca67db4646f856f85405
kuromukade <kuromukade@mail.ru>
2023-02-22 17:49:39 +0300

    input update

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,24 +1,41 @@
-import React, { FC } from "react";
+import React, { FC, useRef, useState } from "react";
+
+import logoIcon from "@/assets/icons/navbar/home.svg";
 
 import styles from "./Input.module.scss";
 
 export enum InputType {
     PASSWORD = "password",
-    TEXT = "text"
+    TEXT = "text",
 }
 
+// e: React.ChangeEvent<HTMLInputElement>
+
 interface InputProps {
+    value: string;
+    setInputValue: (value: string) => void;
     label: string | React.ReactNode;
     placeholder?: string;
     type: InputType;
+    img?: string;
 }
 
-const Input: FC<InputProps> = ({ label, placeholder }) => {
+const Input: FC<InputProps> = ({ label, placeholder, type, img, setInputValue, value }) => {
+
     return (
-    <div className={styles.wrapper}>
-        <span className={styles.label}>{label}</span>
-        <input className={styles.input} type="text" placeholder={placeholder} />
-    </div>
+        <div className={styles.wrapper}>
+            <div className={styles.labelWrapper}>
+                {img && <img className={styles.image} src={logoIcon} alt={`${logoIcon}`} />}    
+                <label className={styles.label}>{label}</label>
+            </div>
+            <input
+                value={value}
+                onChange={(e) => setInputValue(e.target.value)}
+                className={styles.input}
+                type={type}
+                placeholder={placeholder}
+            />
+        </div>
     );
 };
 
--- src/pages/HostRegistrationPages/HostMainInfoPage/HostMainInfoPage.tsx
+++ src/pages/HostRegistrationPages/HostMainInfoPage/HostMainInfoPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 import styles from './HostMainInfoPage.module.scss';
 import MainHeader from "@/components/MainHeader/MainHeader";
@@ -6,14 +6,15 @@ import SideMenu from "@/components/SideMenu/SideMenu";
 import { HostRegistrationSidebarData } from "./HostMainInfoPages.data";
 import { Theme } from "@/components/SideMenu/types/SideMenu.interface";
 import Input, { InputType } from "@/components/ui/Input/Input";
-
+import logoIcon from "@/assets/icons/navbar/home.svg";
 const HostMainInfoPage: FC = () => {
+    const [myInput, setInput] = useState('')
     return (
         <>
             <MainHeader />
             <SideMenu theme={Theme.LIGHT} content={HostRegistrationSidebarData}/>
             <div className={styles.container}>
-                <Input type={InputType.TEXT} label='Адрес' />
+                <Input type={InputType.PASSWORD} label='Адрес' img={logoIcon} value={myInput} setInputValue={setInput} />
             </div>
         </>
     )
