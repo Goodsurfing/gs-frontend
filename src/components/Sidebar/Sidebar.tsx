453f8d115e26044550a092a4058b3964a6c4cfeb
Timur <kuromukade@mail.ru>
2023-03-22 13:11:09 +0300

    removed old sidebar

--- src/components/Sidebar/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC, PropsWithChildren } from "react";
-
-import styles from "./Sidebar.module.scss";
-
-const Sidebar: FC<PropsWithChildren<unknown>> = ({ children }) => {
-    return <div className={styles.sidebar}>{children}</div>;
-};
-
-export default Sidebar;
