dca00c24c7c548f196b7be3e53f72ea242eec78d
kuromukade <kuromukade@mail.ru>
2023-03-28 16:31:03 +0300

    basic wrapper

--- src/pages/OfferCreatePages/OfferCreatePages.tsx
+++ src/pages/OfferCreatePages/OfferCreatePages.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import cn from "classnames";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import SideMenu from "@/components/SideMenu/SideMenu";
+import { Theme } from "@/components/SideMenu/types/SideMenu.interface";
+import MainHeader from "@/components/ui/MainHeader/MainHeader";
+
+import { isMatchUrlEndpoint } from "@/utils/url/isMatchUrlEndpoint";
+
+import { SideMenuData } from "./OfferCreatePages.data";
+import styles from "./OfferCreatePages.module.scss";
 
 const OfferCreatePages = () => {
-  return (
-    <div>OfferCreatePages</div>
-  )
-}
+    const [isOpen, setOpen] = useState<boolean>(false);
+    const { pathname } = useLocation();
 
-export default OfferCreatePages
+    const createContent = (path: string) => {
+        if (isMatchUrlEndpoint(path, "info")) {
+            return <></>;
+        }
+        if (isMatchUrlEndpoint(path, "reset-password")) {
+            return <></>;
+        }
+    };
+
+    return (
+        <div className={styles.layout}>
+            <MainHeader />
+            <SideMenu
+                setOpen={setOpen}
+                isOpen={isOpen}
+                theme={Theme.LIGHT}
+                content={SideMenuData}
+            />
+            <div
+                className={cn(styles.wrapper, {
+                    [styles.opened]: isOpen,
+                })}
+            >
+                {createContent(pathname)}
+            </div>
+        </div>
+    );
+};
+
+export default OfferCreatePages;
---
+++ src/pages/OfferCreatePages/OfferWelcome/OfferWelcome.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./OfferWelcome.module.scss";
+
+const OfferWelcome = () => {
+    return <div>OfferWelcome</div>;
+};
+
+export default OfferWelcome;
