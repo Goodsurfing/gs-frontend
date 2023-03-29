6dbdb95d285a3e253ac68ee81732247fbc281e73
kuromukade <kuromukade@mail.ru>
2023-03-29 16:36:27 +0300

    moved profile to component, add name preload

--- src/components/ui/MainHeader/MainHeader.tsx
+++ src/components/ui/MainHeader/MainHeader.tsx
@@ -1,35 +1,17 @@
-import React, { FC, useRef, useState } from "react";
+import React, { FC } from "react";
 
 import ChangeLanguage from "@/components/ChangeLanguage/ChangeLanguage";
 import LocaleLink from "@/components/LocaleLink/LocaleLink";
 import MobileHeader from "@/components/MobileHeader/MobileHeader";
-import Arrow from "@/components/ui/Arrow/Arrow";
 
 import heartIcon from "@/assets/icons/heart-icon.svg";
 import logotypeIcon from "@/assets/icons/logo-black.svg";
 import messagesIcon from "@/assets/icons/message_icon.svg";
-import defaultAvatarImage from "@/assets/images/default-avatar.jpg";
 
 import styles from "./MainHeader.module.scss";
-import Popup from "@/components/Popup/Popup";
-import { Link } from "react-router-dom";
-import { useOnClickOutside } from "@/hooks/useOnClickOutside";
-import { logout } from "@/store/reducers/loginSlice";
-import { useAppDispatch } from "@/hooks/redux";
+import MainHeaderProfile from "./MainHeaderProfile/MainHeaderProfile";
 
 const MainHeader: FC = () => {
-    const [isProfileOpened, setProfileOpened] = useState<boolean>(false);
-
-    const profileRef = useRef(null);
-
-    const dispatch = useAppDispatch();
-
-    const handleLogout = () => {
-        dispatch(logout());
-    };
-
-    useOnClickOutside(profileRef, () => setProfileOpened(false));
-
     return (
         <>
             <header className={styles.header}>
@@ -48,32 +30,7 @@ const MainHeader: FC = () => {
                             <img src={messagesIcon} alt="Messages" />
                         </LocaleLink>
                     </div>
-                    <div ref={profileRef} onClick={() => setProfileOpened(!isProfileOpened)} className={styles.info}>
-                        <p className={styles.name}>Владислав</p>
-                        <img
-                            src={defaultAvatarImage}
-                            alt="NAME"
-                            className={styles.avatar}
-                        />
-                        <Arrow isOpen={isProfileOpened} />
-                        <Popup className={styles.popup} isOpen={isProfileOpened}>
-                            <Link to='/profile/info'>
-                                Моя страница
-                            </Link>
-                            <Link to='/profile/info'>
-                                Обо мне
-                            </Link>
-                            <Link to='/host/dashboard'>
-                                Дашборд хоста
-                            </Link>
-                            <Link to='/'>
-                                Стать волонтёром
-                            </Link>
-                            <a onClick={handleLogout}>
-                                Выйти
-                            </a>
-                        </Popup>
-                    </div>
+                    <MainHeaderProfile />
                 </div>
             </header>
             <div className={styles.mobile}>
