c5bbe68df1d4377a4261b5d5ae49c1176d5507a4
kuromukade <kuromukade@mail.ru>
2023-08-24 19:02:16 +0300

    birthday fields

--- src/features/ProfileInfo/ui/ProfileInfoFormAboutMe/ProfileInfoFormAboutMe.tsx
+++ src/features/ProfileInfo/ui/ProfileInfoFormAboutMe/ProfileInfoFormAboutMe.tsx
@@ -1,7 +1,10 @@
 import { memo } from "react";
 import cn from "classnames";
+import { useFormContext } from "react-hook-form";
 
 import { useAppSelector } from "@/shared/hooks/redux";
+import { TextAreaControl } from "@/shared/ui/TextAreaControl/TextAreaControl";
+
 import { getProfileReadonly } from "@/entities/Profile";
 
 import styles from "./ProfileInfoFormAboutMe.module.scss";
@@ -13,7 +16,15 @@ interface ProfileInfoFormAboutMeProps {
 export const ProfileInfoFormAboutMe = memo((props: ProfileInfoFormAboutMeProps) => {
     const { className } = props;
     const isLocked = useAppSelector(getProfileReadonly);
+    const { control } = useFormContext();
     return (
-        <div className={cn(className, styles.wrapper)} />
+        <div className={cn(className, styles.wrapper)}>
+            <TextAreaControl
+                disabled={isLocked}
+                control={control}
+                name="aboutMe"
+                label="Расскажите о себе"
+            />
+        </div>
     );
 });
--- src/features/ProfileInfo/ui/ProfileInfoFormBirthDate/ProfileInfoFormBirthDate.tsx
+++ src/features/ProfileInfo/ui/ProfileInfoFormBirthDate/ProfileInfoFormBirthDate.tsx
@@ -1,7 +1,9 @@
 import { memo } from "react";
 import cn from "classnames";
+import { Controller, useFormContext } from "react-hook-form";
 
 import { useAppSelector } from "@/shared/hooks/redux";
+import { SelectComponent } from "@/shared/ui/Select/Select";
 
 import { getProfileReadonly } from "@/entities/Profile";
 
@@ -14,7 +16,18 @@ interface ProfileInfoFormBirthDateProps {
 export const ProfileInfoFormBirthDate = memo((props: ProfileInfoFormBirthDateProps) => {
     const { className } = props;
     const isLocked = useAppSelector(getProfileReadonly);
+    const { control } = useFormContext();
     return (
-        <div className={cn(className, styles.wrapper)} />
+        <div className={cn(className, styles.wrapper)}>
+            <Controller
+                name=""
+                control={control}
+                render={({ field }) => (
+                    <SelectComponent disabled={isLocked} value={field.value}>
+                        
+                    </SelectComponent>
+                )}
+            />
+        </div>
     );
 });
