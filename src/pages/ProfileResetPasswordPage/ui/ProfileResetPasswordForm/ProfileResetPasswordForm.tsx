29956f0d52d9bf1d18b1da86683cb44defb36d2e
kildanL <kildan325@gmail.com>
2023-10-25 18:46:12 +0300

    wip profileResetPassword

--- src/pages/ProfileResetPasswordPage/ui/ProfileResetPasswordForm/ProfileResetPasswordForm.tsx
+++ src/pages/ProfileResetPasswordPage/ui/ProfileResetPasswordForm/ProfileResetPasswordForm.tsx
@@ -1,14 +1,28 @@
 import React, { FC } from "react";
+import {
+    useForm, Controller, useWatch, SubmitHandler,
+} from "react-hook-form";
 import Button from "@/shared/ui/Button/Button";
 
 import { authApi } from "@/store/api/authApi";
 import { userInfoApi } from "@/store/api/userInfoApi";
 
+import styles from "./ProfileResetPasswordForm.module.scss";
+import { IResetPasswordRequestFormData } from "@/types/api/auth/resetPassword.interface";
+
 const ProfileResetPasswordForm: FC = () => {
     const [resetPasswordRequest, { isSuccess }] = authApi.useResetPasswordRequestMutation();
     const { data: userInfo, isSuccess: userInfoSuccess } = userInfoApi.useGetUserInfoQuery();
 
-    const onSubmit: React.MouseEventHandler<HTMLButtonElement> = async () => {
+    const { handleSubmit, control } = useForm();
+
+    const newPassword = useWatch({
+        control,
+        name: "newPassword",
+        defaultValue: "",
+    });
+
+    const onSubmit: SubmitHandler<IResetPasswordRequestFormData> = async ({}) => {
         if (userInfoSuccess && userInfo) {
             await resetPasswordRequest({ email: userInfo.email });
         }
@@ -19,15 +33,61 @@ const ProfileResetPasswordForm: FC = () => {
     }
 
     return (
-        <Button
-            onClick={onSubmit}
-            type="submit"
-            color="BLUE"
-            variant="FILL"
-            size="MEDIUM"
-        >
-            Запросить ссылку
-        </Button>
+        <div className={styles.wrapper}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    name="currentPassword"
+                    control={control}
+                    defaultValue=""
+                    rules={{ required: "Текущий пароль обязателен" }}
+                    render={({ field, fieldState }) => (
+                        <div>
+                            <label>Текущий пароль</label>
+                            <input type="password" {...field} />
+                            {fieldState.error && <p>{fieldState.error.message}</p>}
+                        </div>
+                    )}
+                />
+                <Controller
+                    name="newPassword"
+                    control={control}
+                    defaultValue=""
+                    rules={{ required: "Новый пароль обязателен" }}
+                    render={({ field, fieldState }) => (
+                        <div>
+                            <label>Новый пароль</label>
+                            <input type="password" {...field} />
+                            {fieldState.error && <p>{fieldState.error.message}</p>}
+                        </div>
+                    )}
+                />
+                <Controller
+                    name="repeatNewPassword"
+                    control={control}
+                    defaultValue=""
+                    rules={{
+                        validate: (value) => value === newPassword || "Пароли должны совпадать",
+                        required: "Повторите новый пароль",
+                    }}
+                    render={({ field, fieldState }) => (
+                        <div>
+                            <label>Повторите новый пароль</label>
+                            <input type="password" {...field} />
+                            {fieldState.error && <p>{fieldState.error.message}</p>}
+                        </div>
+                    )}
+                />
+                <Button
+                    type="submit"
+                    color="BLUE"
+                    variant="FILL"
+                    size="MEDIUM"
+                >
+                    Сохранить
+                </Button>
+            </form>
+
+        </div>
     );
 };
 
--- src/pages/ProfileResetPasswordPage/ui/ProfileResetPasswordPage/ProfileResetPasswordPage.tsx
+++ src/pages/ProfileResetPasswordPage/ui/ProfileResetPasswordPage/ProfileResetPasswordPage.tsx
@@ -2,17 +2,22 @@ import React, { FC } from "react";
 
 import ProfileResetPasswordForm from "@/pages/ProfileResetPasswordPage/ui/ProfileResetPasswordForm/ProfileResetPasswordForm";
 
+import { PageLayout } from "@/widgets/PageLayout";
+import { SideMenuData } from "@/shared/data/sidebar/profile-pages";
+
 import styles from "./ProfileResetPasswordPage.module.scss";
 
 const ProfileResetPasswordPage: FC = () => (
-    <main className={styles.main}>
-        <div className={styles.title}>
-            <h2>Изменение пароля</h2>
-        </div>
-        <div className={styles.form}>
-            <ProfileResetPasswordForm />
-        </div>
-    </main>
+    <PageLayout wrapperClassName={styles.layout} sidebarContent={SideMenuData}>
+        <main className={styles.main}>
+            <div className={styles.title}>
+                <h2>Изменение пароля</h2>
+            </div>
+            <div className={styles.form}>
+                <ProfileResetPasswordForm />
+            </div>
+        </main>
+    </PageLayout>
 );
 
 export default ProfileResetPasswordPage;
