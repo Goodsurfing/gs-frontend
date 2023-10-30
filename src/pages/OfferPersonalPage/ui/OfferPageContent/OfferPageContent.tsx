4002b4cbae17a0a0f7d12005e734877228788976
kildanL <kildan325@gmail.com>
2023-10-30 12:21:50 +0300

    move mocked data to seperate file

--- src/pages/OfferPersonalPage/ui/OfferPageContent/OfferPageContent.tsx
+++ src/pages/OfferPersonalPage/ui/OfferPageContent/OfferPageContent.tsx
@@ -1,65 +1,15 @@
 import cn from "classnames";
 import { memo } from "react";
 
-import { Offer, OfferInfoCard } from "@/entities/Offer";
+import { OfferInfoCard } from "@/entities/Offer";
 
+import { mockedOfferData } from "../../model/data/mockedOfferData";
 import styles from "./OfferPageContent.module.scss";
 
 interface OfferPageContentProps {
     className?: string;
     id: string;
 }
-
-const mockedOfferData: Offer = {
-    when: {
-        durationMinDays: 26,
-        durationMaxDays: 30,
-        isApplicableAtTheEnd: true,
-        isFullYearAcceptable: true,
-        isWithoutApplicationEndDate: true,
-        periods: [{ start: "31.01.2023", end: "8.02.2023" }],
-    },
-    where: {
-        address: "Казань",
-    },
-    conditions: {
-        extraFeatures: ["additional", "excursions"],
-        facilities: ["bath", "wi-fi"],
-        payment: { currency: "RUB", contribution: 1600, reward: 2000 },
-        extraConditions: "some conditions",
-        housing: "bed",
-        nutrition: "full",
-        travel: "reimbursement",
-    },
-    whatToDo: {
-        skills: [{ id: 1, text: "cooking" }],
-        workingHours: { dayOff: 3, hours: 40, timeType: "day" },
-        additionalSkills: ["additional skills"],
-        extraInfo: "extra info",
-    },
-    whoNeeds: {
-        gender: "man",
-        languages: ["russian", "english"],
-        receptionPlace: "foreigners",
-        volunteerPlaces: 3,
-        additionalInfo: "additional info",
-        age: "20",
-    },
-    description: {
-        title: "Природный парк «Вулканы Камчатки» ждет волонтеров!",
-        longDescription:
-            "В летнем сезоне 2023 года природному парку «Вулканы Камчатки» требуется помощь добровольцев для выполнения различных видов деятельности на особо охраняемых природных территориях регионального значения. Ждем волонтеров на новый летний сезон! Природный парк «Вулканы Камчатки» объявляет о начале приема заявок на участие в волонтерской программе 2020 года от потенциальных кандидатов. Добровольцы смогут отправиться в природные парки «Налычево», «Быстринский» и «Ключевской». Работы, к которым будут привлекаться волонтеры, запланированы на период с июня по сентябрь. В летнем сезоне 2020 года природному парку «Вулканы Камчатки» требуется помощь добровольцев для выполнения различных видов деятельности на особо охраняемых природных территориях регионального значения. В частности, планируется задействовать волонтеров в работах, связанных с ремонтом и поддержанием в надлежащем виде объектов туристической инфраструктуры, благоустройством прилегающей территории, уборкой мусора, ведением экскурсионной деятельности и общением с посетителями ООПТ регионального значения.",
-        shortDescription: "short description",
-        category: ["category1", "category2"],
-        images: "img1.png",
-    },
-    finishingTouches: {
-        faq: "some faq",
-        rulesInfo: "some rules",
-        welcomeMessage: "welcome message",
-        extraConditions: ["students", "adult-only", "allow-kids", "couples"],
-    },
-};
 
 export const OfferPageContent = memo((props: OfferPageContentProps) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
