df2f3680353a81865c359f1bcb758b08cb077709
kildanL <kildan325@gmail.com>
2023-11-14 15:15:16 +0300

    Add OfferShareCard

--- src/entities/Offer/ui/OfferShareCard/OfferShareCard.tsx
+++ src/entities/Offer/ui/OfferShareCard/OfferShareCard.tsx
@@ -1,9 +1,12 @@
 import React, { FC, memo } from "react";
-
 import { Link } from "react-router-dom";
-import styles from "./OfferShareCard.module.scss";
-import { getMainPageUrl } from "@/shared/config/routes/AppUrls";
+
 import { useLocale } from "@/app/providers/LocaleProvider";
+
+import { getMainPageUrl } from "@/shared/config/routes/AppUrls";
+
+import styles from "./OfferShareCard.module.scss";
+import { instaIcon, vkIcon, telegramIcon } from "@/shared/data/icons/socialIcons";
 
 export const OfferShareCard: FC = memo(() => {
     const { locale } = useLocale();
@@ -11,9 +14,15 @@ export const OfferShareCard: FC = memo(() => {
         <div className={styles.wrapper}>
             <span>Поделиться вакансией</span>
             <div className={styles.container}>
-                <Link to={getMainPageUrl(locale)}>vk</Link>
-                <Link to={getMainPageUrl(locale)}>telegram</Link>
-                <Link to={getMainPageUrl(locale)}>instagram</Link>
+                <Link to={getMainPageUrl(locale)}>
+                    <img src={vkIcon} alt="vkontakte" />
+                </Link>
+                <Link to={getMainPageUrl(locale)}>
+                    <img src={instaIcon} alt="instagram" />
+                </Link>
+                <Link to={getMainPageUrl(locale)}>
+                    <img src={telegramIcon} alt="telegram" />
+                </Link>
             </div>
         </div>
     );
