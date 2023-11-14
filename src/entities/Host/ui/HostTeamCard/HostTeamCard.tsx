1fa3625bbff22e8937ea63de030534308ad5430a
kildanL <kildan325@gmail.com>
2023-11-14 12:11:17 +0300

    add hostTeamCard

--- src/entities/Host/ui/HostTeamCard/HostTeamCard.tsx
+++ src/entities/Host/ui/HostTeamCard/HostTeamCard.tsx
@@ -1,15 +1,30 @@
-import React, { FC, memo } from "react";
 import cn from "classnames";
+import React, { FC, memo, useMemo } from "react";
+
+import { TeamCard } from "@/features/TeamForm";
 import { TeamUser } from "@/features/TeamForm/model/types/team";
+
 import styles from "./HostTeamCard.module.scss";
 
 interface HostTeamCardProps {
-    team: TeamUser[]
+    team: TeamUser[];
 }
 
-export const HostTeamCard: FC<HostTeamCardProps> = memo((props: HostTeamCardProps) => {
-    const {} = props;
-    return (
-        <div>HostTeamCard</div>
-    );
-});
+export const HostTeamCard: FC<HostTeamCardProps> = memo(
+    (props: HostTeamCardProps) => {
+        const { team } = props;
+
+        const renderCard = useMemo(() => team.map((user) => (
+            <TeamCard teamUser={user} disableDeleteIcn key={user.id} />
+        )), [team]);
+
+        return (
+            <div className={cn(styles.wrapper)}>
+                <h3>Команда проекта</h3>
+                <div className={styles.container}>
+                    {renderCard}
+                </div>
+            </div>
+        );
+    },
+);
