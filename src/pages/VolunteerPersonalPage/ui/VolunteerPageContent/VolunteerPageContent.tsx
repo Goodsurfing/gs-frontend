cf46aba128d1301378b7ab2a023ecb11e4d49a3d
kildanL <kildan325@gmail.com>
2023-11-15 12:38:42 +0300

    little fix

--- src/pages/VolunteerPersonalPage/ui/VolunteerPageContent/VolunteerPageContent.tsx
+++ src/pages/VolunteerPersonalPage/ui/VolunteerPageContent/VolunteerPageContent.tsx
@@ -1,20 +1,21 @@
 import React, { FC, memo } from "react";
 
-import styles from "./VolunteerPageContent.module.scss";
 import { VolunteerInfoCard } from "@/entities/Volunteer";
 import { mockedVolunteerData } from "@/entities/Volunteer/model/data/mockedVolunteerData";
+
+import styles from "./VolunteerPageContent.module.scss";
 
 interface VolunteerPageContentProps {
     id: string;
 }
 
-export const VolunteerPageContent: FC<VolunteerPageContentProps> = memo((
-    props: VolunteerPageContentProps,
-) => {
-    const { id } = props;
-    return (
-        <div className={styles.wrapper}>
-            <VolunteerInfoCard volunteer={mockedVolunteerData} />
-        </div>
-    );
-});
+export const VolunteerPageContent: FC<VolunteerPageContentProps> = memo(
+    (props: VolunteerPageContentProps) => {
+        const { id } = props;
+        return (
+            <div className={styles.wrapper}>
+                <VolunteerInfoCard volunteer={mockedVolunteerData} />
+            </div>
+        );
+    },
+);
