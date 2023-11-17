64c6b76a02033daccdad0b26ecfc5b70474810fe
kildanL <kildan325@gmail.com>
2023-11-17 11:30:08 +0300

    add volunteer sidebar data

--- src/pages/VolunteerLayoutPage/ui/VolunteerLayoutPage.tsx
+++ src/pages/VolunteerLayoutPage/ui/VolunteerLayoutPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { PageLayout } from "@/widgets/PageLayout";
+import { SideMenuData } from "@/shared/data/sidebar/volunteer-pages";
 
 const VolunteerLayoutPage = () => (
     <PageLayout sidebarContent={SideMenuData}>
