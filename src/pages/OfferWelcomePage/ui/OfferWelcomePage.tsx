a67f46ddfa0ffc936c234cf896b3237fa24eaa72
kildanL <kildan325@gmail.com>
2023-11-27 16:06:52 +0300

    translate offer welcome page

--- src/pages/OfferWelcomePage/ui/OfferWelcomePage.tsx
+++ src/pages/OfferWelcomePage/ui/OfferWelcomePage.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 
+import { useTranslation } from "react-i18next";
 import { useLocale } from "@/app/providers/LocaleProvider";
 
 import ButtonLink from "@/shared/ui/ButtonLink/ButtonLink";
@@ -9,26 +10,22 @@ import styles from "./OfferWelcomePage.module.scss";
 
 const OfferWelcome: FC = () => {
     const { locale } = useLocale();
-
+    const { t } = useTranslation("offer-welcome");
     return (
         <div className={styles.wrapper}>
-            <h1 className={styles.title}>Привет!</h1>
+            <h1 className={styles.title}>{t("Привет!")}</h1>
             <p className={styles.description}>
-                Здесь вы можете создать и добавить на сайт свое мероприятие. А
-                между тем мы будем предлагать вам полезные советы и примеры.
+                {t("Здесь вы можете создать и добавить на сайт свое мероприятие. А между тем мы будем предлагать вам полезные советы и примеры.")}
             </p>
             <p className={styles.hint}>
-                Теперь на сайте можно проводить мероприятия, поэтому мы рады,
-                что вас заинтересовало это и вы хотите присоединиться к
-                растущему сообществу гудсерферов. Нам не терпится взглянуть, чем
-                вы нас обрадуете.
+                {t("Теперь на сайте можно проводить мероприятия, поэтому мы рады, что вас заинтересовало это и вы хотите присоединиться к растущему сообществу гудсерферов. Нам не терпится взглянуть, чем вы нас обрадуете.")}
             </p>
             <ButtonLink
                 type="primary"
                 path={getOffersWherePageUrl(locale)}
                 className={styles.btn}
             >
-                Начать
+                {t("Начать")}
             </ButtonLink>
         </div>
     );
