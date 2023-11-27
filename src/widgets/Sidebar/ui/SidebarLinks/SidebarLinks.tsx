27cdae6d9b15a02e7e8405e490596897e86d86c5
kildanL <kildan325@gmail.com>
2023-11-27 18:29:52 +0300

    fix lint

--- src/widgets/Sidebar/ui/SidebarLinks/SidebarLinks.tsx
+++ src/widgets/Sidebar/ui/SidebarLinks/SidebarLinks.tsx
@@ -1,11 +1,10 @@
 import { memo, useMemo } from "react";
 
 import cn from "classnames";
-import { useTranslation } from "react-i18next";
-import styles from "./SidebarLinks.module.scss";
 import { SidebarContentProps } from "../../model/types/sidebar";
 import { SidebarLink } from "../SidebarLink/SidebarLink";
 import { SidebarDropdown } from "../SidebarDropdown/SidebarDropdown";
+import styles from "./SidebarLinks.module.scss";
 
 interface SidebarLinksProps {
     content: SidebarContentProps[];
