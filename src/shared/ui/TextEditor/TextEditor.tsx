ada5f84bcc65f5b767d6543417d6e765219d7464
kildanL <kildan325@gmail.com>
2023-12-18 16:50:09 +0300

    add image to text editor

--- src/shared/ui/TextEditor/TextEditor.tsx
+++ src/shared/ui/TextEditor/TextEditor.tsx
@@ -1,21 +1,38 @@
-import React from "react";
-import { useEditor, EditorContent } from "@tiptap/react";
+import BulletList from "@tiptap/extension-bullet-list";
+import Link from "@tiptap/extension-link";
+import ListItem from "@tiptap/extension-list-item";
+import OrderedList from "@tiptap/extension-ordered-list";
+import TextAlign from "@tiptap/extension-text-align";
+import Underline from "@tiptap/extension-underline";
+import Image from "@tiptap/extension-image";
+import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import cn from "classnames";
-import Underline from "@tiptap/extension-underline";
-import styles from "./TextEditor.module.scss";
+import React from "react";
+
 import { ToolBar } from "@/shared/ui/ToolBar/ToolBar";
+
+import styles from "./TextEditor.module.scss";
 
 interface TiptapEditorProps {
     onChange: (content: string) => void;
     value: string;
 }
 
-export const TextEditor: React.FC<TiptapEditorProps> = ({ onChange, value }) => {
+export const TextEditor: React.FC<TiptapEditorProps> = ({
+    onChange,
+    value,
+}) => {
     const editor = useEditor({
         extensions: [
             StarterKit.configure(),
             Underline.configure(),
+            OrderedList,
+            BulletList,
+            ListItem,
+            TextAlign,
+            Link,
+            Image,
         ],
         content: value,
         editorProps: {
--- src/shared/ui/ToolBar/ToolBar.tsx
+++ src/shared/ui/ToolBar/ToolBar.tsx
@@ -1,7 +1,9 @@
 import { HandySvg } from "@handy-ones/handy-svg";
 import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 import { type Editor } from "@tiptap/react";
-import React, { FC, memo, useState } from "react";
+import React, {
+    FC, memo, useCallback, useState,
+} from "react";
 
 import ArrowLeftIcon from "@/shared/assets/icons/textEditor/Arrows1.svg";
 import ArrowRightIcon from "@/shared/assets/icons/textEditor/Arrows2.svg";
@@ -14,8 +16,12 @@ import alignLeftIcon from "@/shared/assets/icons/textEditor/Paragraph1.svg";
 import alignCenterIcon from "@/shared/assets/icons/textEditor/Paragraph2.svg";
 import markIcon from "@/shared/assets/icons/textEditor/Paragraph3.svg";
 import paragraphIcon from "@/shared/assets/icons/textEditor/Paragraph4.svg";
+import bulletListIcon from "@/shared/assets/icons/textEditor/bulletList.svg";
+import imageIcon from "@/shared/assets/icons/textEditor/image.svg";
+import alignJustifyIcon from "@/shared/assets/icons/textEditor/justifyAlign.svg";
 
 import styles from "./ToolBar.module.scss";
+import InputFile from "../InputFile/InputFile";
 
 interface ToolBarProps {
     editor: Editor | null;
@@ -24,6 +30,18 @@ interface ToolBarProps {
 export const ToolBar: FC<ToolBarProps> = memo((props: ToolBarProps) => {
     const { editor } = props;
     const [format, setFormat] = useState(null);
+    const [alignText, setAlignText] = useState(null);
+    const [listItem, setListItem] = useState(null);
+
+    const addImage = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        const fileList = e.target.files;
+        if (fileList && fileList.length > 0) {
+            const file = fileList[0];
+            // console.log(file);
+            const url = URL.createObjectURL(file);
+            editor?.chain().focus().setImage({ src: url }).run();
+        }
+    }, [editor]);
 
     if (!editor) {
         return null;
@@ -40,6 +58,37 @@ export const ToolBar: FC<ToolBarProps> = memo((props: ToolBarProps) => {
                 break;
             case "underlined":
                 editor.chain().toggleUnderline().run();
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleAlignText = (event, newAlign) => {
+        setAlignText(newAlign);
+        switch (newAlign) {
+            case "left":
+                editor.chain().focus().setTextAlign("left").run();
+                break;
+            case "center":
+                editor.chain().focus().setTextAlign("center").run();
+                break;
+            case "justify":
+                editor.chain().focus().setTextAlign("justify").run();
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleListItem = (event, newList) => {
+        setListItem(newList);
+        switch (newList) {
+            case "bullet":
+                editor.chain().toggleBulletList().run();
+                break;
+            case "ordered":
+                editor.chain().toggleOrderedList().run();
                 break;
             default:
                 break;
@@ -76,10 +125,79 @@ export const ToolBar: FC<ToolBarProps> = memo((props: ToolBarProps) => {
                 >
                     <HandySvg src={underlineIcon} />
                 </ToggleButton>
-                {/* <ToggleButton value="color" aria-label="color" disabled>
-                    <HandySvg src={}/>
-                </ToggleButton> */}
             </ToggleButtonGroup>
+            <ToggleButtonGroup
+                exclusive
+                value={alignText}
+                onChange={handleAlignText}
+                aria-label="align text"
+                className={styles.groupWrapper}
+            >
+                <ToggleButton
+                    value="left"
+                    aria-label="left"
+                    className={styles.toggleButton}
+                >
+                    <HandySvg src={alignLeftIcon} />
+                </ToggleButton>
+                <ToggleButton
+                    value="center"
+                    aria-label="center"
+                    className={styles.toggleButton}
+                >
+                    <HandySvg src={alignCenterIcon} />
+                </ToggleButton>
+                <ToggleButton
+                    value="justify"
+                    aria-label="justify"
+                    className={styles.toggleButton}
+                >
+                    <HandySvg src={alignJustifyIcon} />
+                </ToggleButton>
+            </ToggleButtonGroup>
+            <ToggleButtonGroup
+                exclusive
+                value={listItem}
+                onChange={handleListItem}
+                aria-label="list item"
+                className={styles.groupWrapper}
+            >
+                <ToggleButton
+                    value="ordered"
+                    aria-label="ordered"
+                    className={styles.toggleButton}
+                >
+                    <HandySvg src={markIcon} />
+                </ToggleButton>
+                <ToggleButton
+                    value="bullet"
+                    aria-label="bullet"
+                    className={styles.toggleButton}
+                >
+                    <HandySvg src={bulletListIcon} />
+                </ToggleButton>
+            </ToggleButtonGroup>
+            <ToggleButton value="link" className={styles.toggleButton}>
+                <HandySvg src={linkIcon} />
+            </ToggleButton>
+            <ToggleButton
+                value="image"
+                className={styles.toggleButton}
+            >
+                <InputFile
+                    id="upload image"
+                    onChange={addImage}
+                    wrapperClassName={styles.imageButton}
+                    labelClassName={styles.imageButton}
+                    labelChildren={(
+                        <HandySvg src={imageIcon} />
+                    )}
+                />
+
+            </ToggleButton>
+            <ToggleButton value="smile" className={styles.toggleButton}>
+                <HandySvg src={smileIcon} />
+            </ToggleButton>
         </div>
     );
 });
