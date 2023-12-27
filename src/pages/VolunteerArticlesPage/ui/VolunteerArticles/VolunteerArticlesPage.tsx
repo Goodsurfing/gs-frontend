3e163e5c37399087c67ccfea09a4ae3663cbd26a
kildanL <kildan325@gmail.com>
2023-12-27 15:25:47 +0300

    translate volunteer articles page

--- src/pages/VolunteerArticlesPage/ui/VolunteerArticles/VolunteerArticlesPage.tsx
+++ src/pages/VolunteerArticlesPage/ui/VolunteerArticles/VolunteerArticlesPage.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
+import { useTranslation } from "react-i18next";
 import { mockedArticlesData } from "@/entities/Article/model/data/mockedArticleData";
 
 import { ArticlesList } from "../ArticlesList/ArticlesList";
 import styles from "./VolunteerArticlesPage.module.scss";
 
-const VolunteerArticlesPage = () => (
-    <div className={styles.wrapper}>
-        <h2>Мои статьи</h2>
-        <ArticlesList articles={mockedArticlesData} className={styles.container} />
-    </div>
-);
+const VolunteerArticlesPage = () => {
+    const { t } = useTranslation("volunteer");
+    return (
+        <div className={styles.wrapper}>
+            <h2>{t("volunteer-articles.Мои статьи")}</h2>
+            <ArticlesList articles={mockedArticlesData} className={styles.container} />
+        </div>
+    );
+};
 
 export default VolunteerArticlesPage;
