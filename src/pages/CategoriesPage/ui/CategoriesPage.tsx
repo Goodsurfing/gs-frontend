fe03c564df3744d83990896cf4132102d8888af3
kildanL <kildan325@gmail.com>
2024-01-15 13:23:12 +0300

    add categories widget

--- src/pages/CategoriesPage/ui/CategoriesPage.tsx
+++ src/pages/CategoriesPage/ui/CategoriesPage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import { CategoriesWidget } from "@/widgets/CategoriesWidget";
+
 import styles from "./CategoriesPage.module.scss";
 
 const CategoriesPage = () => (
-    <div className={styles.wrapper}>CategoriesPage</div>
+    <div className={styles.wrapper}>
+        <h2 className={styles.title}>Выбирайте по интересам</h2>
+        <CategoriesWidget className={styles.container} />
+    </div>
 );
 
 export default CategoriesPage;
