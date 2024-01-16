17bd4d63ef167dcbd90d6aa63de298288e254eb1
kildanL <kildan325@gmail.com>
2024-01-16 11:40:59 +0300

    add main page layout to categories page

--- src/pages/CategoriesPage/ui/CategoriesPage.tsx
+++ src/pages/CategoriesPage/ui/CategoriesPage.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import { CategoriesWidget } from "@/widgets/CategoriesWidget";
 
 import styles from "./CategoriesPage.module.scss";
+import { MainPageLayout } from "@/widgets/MainPageLayout";
 
 const CategoriesPage = () => (
-    <div className={styles.wrapper}>
-        <h2 className={styles.title}>Выбирайте по интересам</h2>
-        <CategoriesWidget className={styles.container} />
-    </div>
+    <MainPageLayout>
+        <div className={styles.wrapper}>
+            <h2 className={styles.title}>Выбирайте по интересам</h2>
+            <CategoriesWidget className={styles.container} />
+        </div>
+    </MainPageLayout>
 );
 
 export default CategoriesPage;
