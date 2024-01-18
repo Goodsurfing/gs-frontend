d2ed0ac01c42abccf19db3636308256fade88fb6
kildanL <kildan325@gmail.com>
2024-01-18 13:53:52 +0300

    add empty page

---
+++ src/pages/NPOPage/ui/NPOPage/NPOPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styles from "./NPOPage.module.scss";
+import { MainPageLayout } from "@/widgets/MainPageLayout";
+
+const NPOPage = () => (
+    <MainPageLayout>
+        <div className={styles.wrapper}>NPOPage</div>
+    </MainPageLayout>
+);
+
+export default NPOPage;
