0df49448caf6261ccb33389cf7b9860f7ab4d658
kildanL <kildan325@gmail.com>
2024-01-22 17:07:03 +0300

    add empty page

---
+++ src/pages/OurTeamPage/ui/OurTeamPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styles from "./OurTeamPage.module.scss";
+import { MainPageLayout } from "@/widgets/MainPageLayout";
+
+const OurTeamPage = () => (
+    <MainPageLayout>
+        <div className={styles.wrapper}>OurTeam</div>
+    </MainPageLayout>
+);
+
+export default OurTeamPage;
