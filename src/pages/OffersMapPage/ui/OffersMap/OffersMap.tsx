c2924a7735a80b303e07a614aeda6bb6f4285b75
kildanL <kildan325@gmail.com>
2024-04-08 15:19:30 +0300

    wip change map ballon logic

--- src/pages/OffersMapPage/ui/OffersMap/OffersMap.tsx
+++ src/pages/OffersMapPage/ui/OffersMap/OffersMap.tsx
@@ -1,16 +1,27 @@
-import React, { FC, useState } from "react";
-import cn from "classnames";
 import { Placemark } from "@pbe/react-yandex-maps";
-import { YMap, YmapType } from "@/entities/Map";
+import cn from "classnames";
+import React, {
+    FC, useEffect, useRef, useState,
+} from "react";
+
 import { validateCoordinates } from "@/features/MapWithAddress";
+
+import { YMap, YmapType } from "@/entities/Map";
+
 import defaultImage from "@/shared/assets/images/personalCardMOCK.png";
-import styles from "./OffersMap.module.scss";
+import useTimeout from "@/shared/hooks/useTimeout";
 import { Portal } from "@/shared/ui/Portal/Portal";
+
 import { OfferBalloon } from "../OfferBalloon/OfferBalloon";
+import styles from "./OffersMap.module.scss";
 import "./yandex-map-restyle-ballon.scss";
 
 interface OffersMapProps {
     className?: string;
+}
+
+interface GeometryType {
+    geometry: number[][];
 }
 
 export const OffersMap: FC<OffersMapProps> = (props) => {
@@ -19,6 +30,11 @@ export const OffersMap: FC<OffersMapProps> = (props) => {
     const [ymap, setYmap] = useState<YmapType | undefined>(undefined);
     const [loading, setLoading] = useState(false);
     const [activePortal, setActivePortal] = useState(false);
+    const testData = [
+        { geometry: [55.788028, 49.121729] },
+        { geometry: [55.78979, 49.117149] },
+        { geometry: [55.788824, 49.114648] },
+    ];
 
     const handlePlacemarkClick = (offer) => {
         setSelectedOffer(offer);
@@ -28,8 +44,8 @@ export const OffersMap: FC<OffersMapProps> = (props) => {
         <div className={cn(className, styles.wrapper)}>
             <YMap
                 mapState={{
-                    center: [55.847973, 37.692542],
-                    zoom: 10,
+                    center: [55.788273, 49.1194],
+                    zoom: 15,
                 }}
                 className={cn(styles.map, {
                     [styles.loading]: !loading,
@@ -39,18 +55,36 @@ export const OffersMap: FC<OffersMapProps> = (props) => {
                 modules={["geoObject.addon.balloon"]}
             >
                 <div className={styles.customPlacemark}>
-                    <Placemark
-                        geometry={[55.847973, 37.692542]}
-                        properties={{ balloonContent: "<div id=\"balloon-offer\" class=\"offer-card\"></div>" }}
-                        onClick={() => {
-                            setTimeout(() => { setActivePortal(true); }, 0);
-                        }}
-                    />
-                    {activePortal && (
-                        <Portal className={styles.portal} elementId="balloon-offer">
-                            <OfferBalloon title="Приют животных в петербурге" cover={defaultImage} />
+                    {testData.map((test) => (
+                        <Placemark
+                            geometry={test.geometry}
+                            properties={{
+                                balloonContent:
+                                    "<div id=\"balloon-offer\" class=\"offer-card\">ПРИВЕТ!!!</div>",
+                            }}
+                            options={{
+                                hideIconOnBalloonOpen: false,
+                                openEmptyBalloon: false,
+
+                            }}
+                            onClick={() => {
+                                setTimeout(() => {
+                                    setActivePortal(true);
+                                }, 0);
+                            }}
+                        />
+                    ))}
+                    {/* {activePortal && (
+                        <Portal
+                            className={styles.portal}
+                            elementId="balloon-offer"
+                        >
+                            <OfferBalloon
+                                title="Приют животных в петербурге"
+                                cover={defaultImage}
+                            />
                         </Portal>
-                    )}
+                    )} */}
                 </div>
             </YMap>
         </div>
