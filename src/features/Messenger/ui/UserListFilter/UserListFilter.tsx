9695447ff31b89d7fa2e5cf4dba03b459d0a445f
kildanL <kildan325@gmail.com>
2024-05-15 15:23:56 +0300

    searchInput moved to shared

--- src/features/Messenger/ui/UserListFilter/UserListFilter.tsx
+++ src/features/Messenger/ui/UserListFilter/UserListFilter.tsx
@@ -2,13 +2,15 @@ import React, { useState, FC } from "react";
 import styles from "./UserListFilter.module.scss";
 import { OfferState } from "@/entities/Offer";
 
+type UserListTypes = OfferState | null;
+
 interface UserListFilterProps {
     onChange?: () => void;
 }
 
 export const UserListFilter: FC<UserListFilterProps> = (props) => {
     const { onChange } = props;
-    const [filterValue, setFilterValue] = useState<OfferState>(null);
+    const [filterValue, setFilterValue] = useState<UserListTypes>(null);
     return (
         <div className={styles.wrapepr}>UserListFilter</div>
     );
