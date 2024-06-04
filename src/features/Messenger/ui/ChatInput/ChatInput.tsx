c956b7a124becbdac5bcbc44aa9c36d2749ae0eb
kildanL <kildan325@gmail.com>
2024-06-04 13:06:31 +0300

    add emoji picker

--- src/features/Messenger/ui/ChatInput/ChatInput.tsx
+++ src/features/Messenger/ui/ChatInput/ChatInput.tsx
@@ -1,13 +1,15 @@
-import React, { FC } from "react";
+import React, { FC, useRef, useState } from "react";
 import {
     IconButton, InputBase, Paper, SxProps, Theme,
 } from "@mui/material";
 import { ReactSVG } from "react-svg";
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
 import clipIcon from "@/shared/assets/icons/clip.svg";
 import smileIcon from "@/shared/assets/icons/textEditor/smile.svg";
 import sendIcon from "@/shared/assets/icons/send-arrow.svg";
 
 import styles from "./ChatInput.module.scss";
+import { useOnClickOutside } from "@/shared/hooks/useOnClickOutside";
 
 interface ChatInputProps {
     sx?: SxProps<Theme>;
@@ -15,6 +17,17 @@ interface ChatInputProps {
 
 export const ChatInput: FC<ChatInputProps> = (props) => {
     const { sx } = props;
+    const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
+
+    const emojiRef = useRef(null);
+
+    useOnClickOutside(emojiRef, () => setShowEmojiPicker((prev) => !prev));
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const onEmojiClick = (event: EmojiClickData) => {
+
+    };
+
     return (
         <div className={styles.wrapper}>
             <Paper
@@ -35,9 +48,23 @@ export const ChatInput: FC<ChatInputProps> = (props) => {
                     <ReactSVG src={clipIcon} className={styles.clip} />
                 </IconButton>
                 <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Написать сообщение" />
-                <IconButton aria-label="menu" className={styles.button}>
+                {/* <div className={styles.emojiWrapper}> */}
+                <IconButton
+                    aria-label="menu"
+                    className={styles.button}
+                    onClick={() => setShowEmojiPicker((prev) => !prev)}
+                >
                     <ReactSVG src={smileIcon} className={styles.icon} />
+                    {showEmojiPicker && (
+                        <div className={styles.emojiModal} ref={emojiRef}>
+                            <EmojiPicker
+                                onEmojiClick={onEmojiClick}
+                                lazyLoadEmojis
+                            />
+                        </div>
+                    )}
                 </IconButton>
+                {/* </div> */}
             </Paper>
             <IconButton aria-label="menu" className={styles.button}>
                 <ReactSVG src={sendIcon} className={styles.icon} />
---
+++ src/features/Messenger/ui/OfferApplication/OfferApplication.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import styles from "./OfferApplication.module.scss";
+
+interface OfferApplicationProps {
+    onChange?: () => void
+}
+
+export const OfferApplication: FC<OfferApplicationProps> = (props) => {
+    const { onChange } = props;
+    return (
+        <div className={styles.wrapper}>OfferApplication</div>
+    );
+};
