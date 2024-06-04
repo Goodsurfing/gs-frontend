951a32fee6cff481d6e3e6046358b81d2641fad4
kildanL <kildan325@gmail.com>
2024-06-04 15:35:26 +0300

    fix and update emojipicker

--- src/features/Messenger/ui/ChatInput/ChatInput.tsx
+++ src/features/Messenger/ui/ChatInput/ChatInput.tsx
@@ -1,12 +1,14 @@
-import React, { FC, useRef, useState } from "react";
 import {
     IconButton, InputBase, Paper, SxProps, Theme,
 } from "@mui/material";
-import { ReactSVG } from "react-svg";
 import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
+import { MouseDownEvent } from "emoji-picker-react/dist/config/config";
+import React, { FC, useRef, useState } from "react";
+import { ReactSVG } from "react-svg";
+
 import clipIcon from "@/shared/assets/icons/clip.svg";
-import smileIcon from "@/shared/assets/icons/textEditor/smile.svg";
 import sendIcon from "@/shared/assets/icons/send-arrow.svg";
+import smileIcon from "@/shared/assets/icons/textEditor/smile.svg";
 
 import styles from "./ChatInput.module.scss";
 import { useOnClickOutside } from "@/shared/hooks/useOnClickOutside";
@@ -18,20 +20,22 @@ interface ChatInputProps {
 export const ChatInput: FC<ChatInputProps> = (props) => {
     const { sx } = props;
     const [showEmojiPicker, setShowEmojiPicker] = useState<boolean>(false);
-
     const emojiRef = useRef(null);
 
-    useOnClickOutside(emojiRef, () => setShowEmojiPicker((prev) => !prev));
+    useOnClickOutside(emojiRef, () => setShowEmojiPicker(() => false));
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const onEmojiClick = (event: EmojiClickData) => {
-
-    };
+    function onEmojiClick(emoji: EmojiClickData, event: MouseEvent): void {
+        // event.stopPropagation();
+    }
 
     return (
         <div className={styles.wrapper}>
             <Paper
                 component="form"
+                onClick={(event) => {
+                    event.stopPropagation();
+                }}
                 sx={{
                     p: "10px 14px",
                     display: "flex",
@@ -47,24 +51,32 @@ export const ChatInput: FC<ChatInputProps> = (props) => {
                 <IconButton aria-label="menu" className={styles.button}>
                     <ReactSVG src={clipIcon} className={styles.clip} />
                 </IconButton>
-                <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Написать сообщение" />
-                {/* <div className={styles.emojiWrapper}> */}
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Написать сообщение"
+                />
                 <IconButton
+                    ref={emojiRef}
                     aria-label="menu"
                     className={styles.button}
-                    onClick={() => setShowEmojiPicker((prev) => !prev)}
+                    onClick={() => {
+                        setShowEmojiPicker((prev) => !prev);
+                    }}
                 >
                     <ReactSVG src={smileIcon} className={styles.icon} />
                     {showEmojiPicker && (
-                        <div className={styles.emojiModal} ref={emojiRef}>
+                        <div
+                            className={styles.emojiWrapper}
+                            onClick={(e) => e.stopPropagation()}
+                            ref={emojiRef}
+                        >
                             <EmojiPicker
-                                onEmojiClick={onEmojiClick}
+                                onEmojiClick={(emoji, event) => onEmojiClick(emoji, event)}
                                 lazyLoadEmojis
                             />
                         </div>
                     )}
                 </IconButton>
-                {/* </div> */}
             </Paper>
             <IconButton aria-label="menu" className={styles.button}>
                 <ReactSVG src={sendIcon} className={styles.icon} />
