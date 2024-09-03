0cd12e64a271cd3955d82af48aada3433a7ad9d0
kildanL <kildan325@gmail.com>
2024-09-03 16:03:52 +0300

    wip connect profile avatar

--- src/features/ProfileInfo/ui/ProfileInfoFormAvatar/ProfileInfoFormAvatar.tsx
+++ src/features/ProfileInfo/ui/ProfileInfoFormAvatar/ProfileInfoFormAvatar.tsx
@@ -1,7 +1,12 @@
-import { memo } from "react";
 import cn from "classnames";
+import { memo, useState } from "react";
+import ProfileInput from "@/components/ProfileInput/ProfileInput";
+
+import { useLocale } from "@/app/providers/LocaleProvider";
 
 import { getProfileReadonly } from "@/entities/Profile";
+
+import { getProfileInfoPageUrl } from "@/shared/config/routes/AppUrls";
 import { useAppSelector } from "@/shared/hooks/redux";
 
 import styles from "./ProfileInfoFormAvatar.module.scss";
@@ -10,12 +15,25 @@ interface ProfileInfoFormAvatarProps {
     className?: string;
 }
 
-export const ProfileInfoFormAvatar = memo((props: ProfileInfoFormAvatarProps) => {
-    const { className } = props;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const isLocked = useAppSelector(getProfileReadonly);
+export const ProfileInfoFormAvatar = memo(
+    (props: ProfileInfoFormAvatarProps) => {
+        const { className } = props;
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const isLocked = useAppSelector(getProfileReadonly);
+        const [avatar, setAvatar] = useState<File>();
+        const { locale } = useLocale();
 
-    return (
-        <div className={cn(className, styles.wrapper)} />
-    );
-});
+        return (
+            <div className={cn(className, styles.wrapper)}>
+                <ProfileInput
+                    fileClassname={styles.fileInput}
+                    className={className}
+                    id="profile-file"
+                    file={avatar}
+                    setFile={setAvatar}
+                    route={getProfileInfoPageUrl(locale)}
+                />
+            </div>
+        );
+    },
+);
