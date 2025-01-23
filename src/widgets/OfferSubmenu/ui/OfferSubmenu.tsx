2fa8cb792c0565071ba2a76b2fe7cdb2973e6e62
kildanl <kildan325@gmail.com>
2025-01-23 20:28:17 +0300

    little fix

--- src/widgets/OfferSubmenu/ui/OfferSubmenu.tsx
+++ src/widgets/OfferSubmenu/ui/OfferSubmenu.tsx
@@ -1,42 +1,42 @@
 import React, { FC } from "react";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 import styles from "./OfferSubmenu.module.scss";
 import { Submenu } from "@/widgets/Submenu";
 import Button from "@/shared/ui/Button/Button";
 import { useTranslateSubmenu } from "@/shared/hooks/useTranslateSubmenu";
+import { getMessengerPageCreateUrl } from "@/shared/config/routes/AppUrls";
+import { useLocale } from "@/app/providers/LocaleProvider";
 
 interface OfferSubmenuProps {
     id?: string;
 }
 
 export const OfferSubmenu: FC<OfferSubmenuProps> = (props) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { id } = props;
     const { t } = useTranslation("offer");
     const { SubmenuItemsOffer } = useTranslateSubmenu();
+    const navigate = useNavigate();
+    const { locale } = useLocale();
+
+    const navigateTo = () => {
+        navigate(getMessengerPageCreateUrl(locale, "create", id));
+    };
+
     return (
         <Submenu
             className={styles.navMenu}
             items={SubmenuItemsOffer}
             buttons={(
-                <>
-                    <Button
-                        size="SMALL"
-                        color="BLUE"
-                        variant="OUTLINE"
-                        className={styles.button}
-                    >
-                        {t("personalOffer.Написать")}
-                    </Button>
-                    <Button
-                        size="SMALL"
-                        color="BLUE"
-                        variant="FILL"
-                        className={styles.button}
-                    >
-                        {t("personalOffer.Участвовать")}
-                    </Button>
-                </>
+                <Button
+                    size="SMALL"
+                    color="BLUE"
+                    variant="FILL"
+                    className={styles.button}
+                    onClick={navigateTo}
+                >
+                    {t("personalOffer.Участвовать")}
+                </Button>
             )}
         />
     );
