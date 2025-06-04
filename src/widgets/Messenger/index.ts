59e6b4261b0eb5cb55aac09e2c252c50cba79d8d
kildanL <kildan325@gmail.com>
2025-06-04 13:26:19 +0300

    fix typescript error

--- src/widgets/Messenger/index.ts
+++ src/widgets/Messenger/index.ts
@@ -1,4 +1,3 @@
 export { MessengerList } from "./ui/MessengerList/MessengerList";
 export { ListFilter } from "./ui/ListFilter/ListFilter";
 export { Chat } from "./ui/Chat/Chat";
-export type { ReadRef } from "./ui/MessengerList/MessengerList";
