d48f3558604ed2886bb1e8f1c4b7b9242e28762a
kildan <kildan325@gmail.com>
2026-06-11 12:55:10 +0300

    feat: add SEO metadata for donation rating page

--- src/pages/DonationRatingPage/ui/DonationRatingPage/DonationRatingPage.tsx
+++ src/pages/DonationRatingPage/ui/DonationRatingPage/DonationRatingPage.tsx
@@ -1,20 +1,40 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
+import { useLocale } from "@/app/providers/LocaleProvider";
+import { getDonationRating } from "@/shared/config/routes/AppUrls";
+import { getSeoUrl } from "@/shared/lib/getSeoUrl";
+import { SeoHelmet } from "@/shared/ui/SeoHelmet";
 import { MainPageLayout } from "@/widgets/MainPageLayout";
 
+import { DonationRating } from "../DonationRating/DonationRating";
 import { Header } from "../Header/Header";
 import styles from "./DonationRatingPage.module.scss";
-import { DonationRating } from "../DonationRating/DonationRating";
 
-const DonationRatingPage = () => (
-    <MainPageLayout>
-        <div className={styles.wrapper}>
-            <Header />
-            <div className={styles.content}>
-                <DonationRating />
+const DonationRatingPage = () => {
+    const { locale } = useLocale();
+    const { t, ready } = useTranslation("donation");
+
+    return (
+        <MainPageLayout>
+            {ready && (
+                <SeoHelmet
+                    title={t("ratingSeo.title")}
+                    description={t("ratingSeo.description")}
+                    canonicalUrl={getSeoUrl(getDonationRating(locale))}
+                    keywords={t("ratingSeo.keywords")}
+                    ogTitle={t("ratingSeo.ogTitle")}
+                    ogDescription={t("ratingSeo.ogDescription")}
+                />
+            )}
+            <div className={styles.wrapper}>
+                <Header />
+                <div className={styles.content}>
+                    <DonationRating />
+                </div>
             </div>
-        </div>
-    </MainPageLayout>
-);
+        </MainPageLayout>
+    );
+};
 
 export default DonationRatingPage;
