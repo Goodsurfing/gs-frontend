b4c96af929e55daea9ed0e34204493e15ff3ca97
kildan <kildan325@gmail.com>
2026-06-11 11:15:51 +0300

    feat: add SEO metadata for ambassadors page

--- src/pages/GoodsurfingAmbassadorsPage/ui/GoodsurfingAmbassadorsPage/GoodsurfingAmbassadorsPage.tsx
+++ src/pages/GoodsurfingAmbassadorsPage/ui/GoodsurfingAmbassadorsPage/GoodsurfingAmbassadorsPage.tsx
@@ -1,22 +1,42 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
+import { useLocale } from "@/app/providers/LocaleProvider";
 import { MainPageLayout } from "@/widgets/MainPageLayout";
+import { getAmbassadorsPageUrl } from "@/shared/config/routes/AppUrls";
+import { getSeoUrl } from "@/shared/lib/getSeoUrl";
+import { SeoHelmet } from "@/shared/ui/SeoHelmet";
 
 import { Ambassador } from "../Ambassador/Ambassador";
 import { Header } from "../Header/Header";
 import { Text } from "../Text/Text";
 import styles from "./GoodsurfingAmbassadorsPage.module.scss";
 
-const GoodsurfingAmbassadorsPage = () => (
-    <MainPageLayout>
-        <div className={styles.wrapper}>
-            <Header />
-            <div className={styles.content}>
-                <Text />
-                <Ambassador />
+const GoodsurfingAmbassadorsPage = () => {
+    const { locale } = useLocale();
+    const { t, ready } = useTranslation("ambassadors");
+
+    return (
+        <MainPageLayout>
+            {ready && (
+                <SeoHelmet
+                    title={t("seo.title")}
+                    description={t("seo.description")}
+                    canonicalUrl={getSeoUrl(getAmbassadorsPageUrl(locale))}
+                    keywords={t("seo.keywords")}
+                    ogTitle={t("seo.ogTitle")}
+                    ogDescription={t("seo.ogDescription")}
+                />
+            )}
+            <div className={styles.wrapper}>
+                <Header />
+                <div className={styles.content}>
+                    <Text />
+                    <Ambassador />
+                </div>
             </div>
-        </div>
-    </MainPageLayout>
-);
+        </MainPageLayout>
+    );
+};
 
 export default GoodsurfingAmbassadorsPage;
