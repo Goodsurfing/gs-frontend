dd5d9076566b33719ad17eaeddb339767c9d0ddd
kildan <kildan325@gmail.com>
2026-06-11 15:15:33 +0300

    feat: add SEO metadata for team page

--- src/pages/OurTeamPage/ui/OurTeamPage/OurTeamPage.tsx
+++ src/pages/OurTeamPage/ui/OurTeamPage/OurTeamPage.tsx
@@ -1,21 +1,34 @@
 import React from "react";
 
 import { useTranslation } from "react-i18next";
+import { useLocale } from "@/app/providers/LocaleProvider";
+import { getOurTeamPageUrl } from "@/shared/config/routes/AppUrls";
+import { getSeoUrl } from "@/shared/lib/getSeoUrl";
+import ButtonLink from "@/shared/ui/ButtonLink/ButtonLink";
+import { SeoHelmet } from "@/shared/ui/SeoHelmet";
 import { MainPageLayout } from "@/widgets/MainPageLayout";
-
-// import Button from "@/shared/ui/Button/Button";
 
 import { Founders } from "../Founders/Founders";
 import { GoodsurfingTeam } from "../GoodsurfingTeam/GoodsurfingTeam";
 import { Header } from "../Header/Header";
 import styles from "./OurTeamPage.module.scss";
-import ButtonLink from "@/shared/ui/ButtonLink/ButtonLink";
 
 const OurTeamPage = () => {
-    const { t } = useTranslation("our-team");
+    const { locale } = useLocale();
+    const { t, ready } = useTranslation("our-team");
 
     return (
         <MainPageLayout>
+            {ready && (
+                <SeoHelmet
+                    title={t("seo.title")}
+                    description={t("seo.description")}
+                    canonicalUrl={getSeoUrl(getOurTeamPageUrl(locale))}
+                    keywords={t("seo.keywords")}
+                    ogTitle={t("seo.ogTitle")}
+                    ogDescription={t("seo.ogDescription")}
+                />
+            )}
             <div className={styles.wrapper}>
                 <Header />
                 <div className={styles.content}>
