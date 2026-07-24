108a8dbb1c824c136de22c6959723b0e52d030f4
Roman <roman@macmini.local>
2026-07-25 00:45:47 +0300

    Redesign preloader around the GoodSurfing logo
    Replaces the generic rotating-circles spinner with the brand mark
    gently bobbing above a looping teal wave line (nods to "surfing").

--- src/shared/ui/Preloader/Preloader.tsx
+++ src/shared/ui/Preloader/Preloader.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import cn from "classnames";
 
+import logoIcon from "@/shared/assets/icons/logo-black.svg";
 import styles from "./Preloader.module.scss";
 
 interface PreloaderProps {
@@ -12,24 +13,23 @@ interface PreloaderProps {
 const Preloader = ({ className, preloader }: PreloaderProps) => (
     <div className={cn(styles.layout, className)}>
         <div className={cn(preloader, styles.preloader)}>
-            <svg
-                viewBox="0 0 102 102"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    className={styles.bigCircle}
-                    d="M101 51C101 78.6142 78.6142 101 51 101C23.3858 101 1 78.6142 1 51"
-                    stroke="#252525"
-                    strokeWidth="2"
-                />
-                <path
-                    className={styles.smallCircle}
-                    d="M91 51C91 28.9086 73.0914 11 51 11C28.9086 11 11 28.9086 11 51"
-                    stroke="#252525"
-                    strokeWidth="2"
-                />
-            </svg>
+            <img src={logoIcon} alt="GoodSurfing" className={styles.logo} />
+            <div className={styles.waveViewport}>
+                <svg
+                    className={styles.wave}
+                    viewBox="0 0 240 24"
+                    fill="none"
+                    preserveAspectRatio="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M0 12 C 15 2, 45 2, 60 12 C 75 22, 105 22, 120 12 C 135 2, 165 2, 180 12 C 195 22, 225 22, 240 12"
+                        stroke="#00A0A8"
+                        strokeWidth="2.5"
+                        strokeLinecap="round"
+                    />
+                </svg>
+            </div>
         </div>
     </div>
 );
